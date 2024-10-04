2f11a62396ab9f03fef79a5931d54e7209356751
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-04 16:26:12 +0530

    root check

---
+++ roboshop-shell-shiva/1-web.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# echo "File Name: $0"
+# echo "First Parameter : $1"
+# echo "Second Parameter : $2"
+# echo "Quoted Values: $@"
+# echo "Quoted Values: $*"
+# echo "Total Number of Parameters : $#"
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+# check root user
+set -x
+
+ISROOT() {
+    echo "checking if current user is root user"
+    if [ $ID -eq 0 ]; then
+        echo -e "$G[SUCCESS] Current user is root user$N"
+    else
+        echo -e "$R[FAIL] Current user is not root user$N"
+    fi
+}
