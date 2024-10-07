c4602b6e9894f549eba4726b8e9529c222ff582c
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-10-07 09:19:05 +0530

    added logs

--- 10payment.sh
+++ 10payment.sh
@@ -6,77 +6,75 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$0-"$TIMESTAMP".log
+ID=$(id -u) &>>"$LOGFILE"
 
-ID=$(id -u) &>> "$LOGFILE"
-
-ROOTCHECK(){
+ROOTCHECK() {
   echo
   if [ "$ID" -eq 0 ]; then
     echo -e "$G Root user $N"
   else
-    echo  -e "$R Not root user $N"
+    echo -e "$R Not root user $N"
     exit 1
   fi
-  }
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE=/tmp/$0-"$TIMESTAMP".log
+}
 
-VALIDATE(){
+VALIDATE() {
 
   if [ "$1" -eq 0 ]; then
-        echo -e "$2" "$G is Successful $N"
+    echo -e "$2" "$G is Successful $N"
   else
-        echo -e "$2" "$R Failed $N"
+    echo -e "$2" "$R Failed $N"
   fi
-  }
+}
 
-ROOTCHECK &>> "$LOGFILE"
+ROOTCHECK &>>"$LOGFILE"
 VALIDATE $? "checking if root "
-
 
 #Payment
 #This service is responsible for payments in RoboShop e-commerce app. This service is written on Python 3.6, So need it to run this app.
 #Install Python 3.6
 
-dnf install python36 gcc python3-devel -y &>> "$LOGFILE"
+dnf install python36 gcc python3-devel -y &>>"$LOGFILE"
 VALIDATE $? "install python gcc python3 "
 #Configure the application.
 #Add application User
 
-useradd roboshop &>> "$LOGFILE"
+useradd roboshop &>>"$LOGFILE"
 VALIDATE $? "create roboshop user "
 #Lets setup an app directory.
 
-mkdir /app &>> "$LOGFILE"
+mkdir /app &>>"$LOGFILE"
 VALIDATE $? "create directory app "
 #Download the application code to created app directory.
 
-curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> "$LOGFILE"
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>>"$LOGFILE"
 VALIDATE $? "download the payment archive "
-cd /app &>> "$LOGFILE"
+cd /app &>>"$LOGFILE"
 VALIDATE $? "navigate to app directory "
-unzip /tmp/payment.zip &>> "$LOGFILE"
+unzip /tmp/payment.zip &>>"$LOGFILE"
 VALIDATE $? "inflate payment archive "
 #Every application is developed by development team will have some common softwares that they use as libraries. This application also have the same way of defined dependencies in the application configuration.
 #Lets download the dependencies.
 
-cd /app &>> "$LOGFILE"
+cd /app &>>"$LOGFILE"
 VALIDATE $? "navigate to directory app "
-pip3.6 install -r requirements.txt &>> "$LOGFILE"
+pip3.6 install -r requirements.txt &>>"$LOGFILE"
 VALIDATE $? "install python requirements "
 #We need to setup a new service in systemd so systemctl can manage this service
 #Setup SystemD Payment Service
 
-{ cp /root/roboshop-shell/configuration/payment.service /etc/systemd/system/payment.service; } &>> "$LOGFILE"
+{ cp /root/roboshop-shell/configuration/payment.service /etc/systemd/system/payment.service; } &>>"$LOGFILE"
 VALIDATE $? "copy payment service "
 #Load the service.
 
-systemctl daemon-reload &>> "$LOGFILE"
+systemctl daemon-reload &>>"$LOGFILE"
 VALIDATE $? "reload daemon "
 #Start the service.
 
-systemctl enable payment &>> "$LOGFILE"
+systemctl enable payment &>>"$LOGFILE"
 VALIDATE $? "enable the payment "
-systemctl start payment &>> "$LOGFILE"
+systemctl start payment &>>"$LOGFILE"
 VALIDATE $? "start the payment app "
 echo -e "$Y payment Application installed successfully $N"
--- 11dispatch.sh
+++ 11dispatch.sh
@@ -6,86 +6,83 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$0-"$TIMESTAMP".log
+ID=$(id -u) &>>"$LOGFILE"
 
-ID=$(id -u) &>> "$LOGFILE"
-
-ROOTCHECK(){
+ROOTCHECK() {
   echo
   if [ "$ID" -eq 0 ]; then
     echo -e "$G Root user $N"
   else
-    echo  -e "$R Not root user $N"
+    echo -e "$R Not root user $N"
     exit 1
   fi
-  }
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE=/tmp/$0-"$TIMESTAMP".log
+}
 
-VALIDATE(){
+VALIDATE() {
 
   if [ "$1" -eq 0 ]; then
-        echo -e "$2" "$G is Successful $N"
+    echo -e "$2" "$G is Successful $N"
   else
-        echo -e "$2" "$R Failed $N"
+    echo -e "$2" "$R Failed $N"
   fi
-  }
+}
 
-ROOTCHECK &>> "$LOGFILE"
+ROOTCHECK &>>"$LOGFILE"
 VALIDATE $? "checking if root "
-
-
 
 #Dispatch
 #Dispatch is the service which dispatches the product after purchase. It is written in GoLang, So wanted to install GoLang.
 #Install GoLang
 
-dnf install golang -y &>> "$LOGFILE"
+dnf install golang -y &>>"$LOGFILE"
 VALIDATE $? "install golang "
 
 #Configure the application.
 
 #Add application User
 
-useradd roboshop &>> "$LOGFILE"
+useradd roboshop &>>"$LOGFILE"
 VALIDATE $? "add user roboshop "
 #Lets setup an app directory.
 
-mkdir /app &>> "$LOGFILE"
+mkdir /app &>>"$LOGFILE"
 VALIDATE $? "created directory app "
 #Download the application code to created app directory.
 
-curl -L -o /tmp/dispatch.zip https://roboshop-builds.s3.amazonaws.com/dispatch.zip &>> "$LOGFILE"
+curl -L -o /tmp/dispatch.zip https://roboshop-builds.s3.amazonaws.com/dispatch.zip &>>"$LOGFILE"
 VALIDATE $? "download dispatch "
-cd /app &>> "$LOGFILE"
+cd /app &>>"$LOGFILE"
 VALIDATE $? "created app directory "
-unzip /tmp/dispatch.zip &>> "$LOGFILE"
+unzip /tmp/dispatch.zip &>>"$LOGFILE"
 VALIDATE $? "unzip dispatch "
 #Every application is developed by development team will have some common softwares that they use as libraries. This application also have the same way of defined dependencies in the application configuration.
 #Lets download the dependencies & build the software.
 
-cd /app &>> "$LOGFILE"
+cd /app &>>"$LOGFILE"
 VALIDATE $? "create app directory "
-go mod init dispatch &>> "$LOGFILE"
+go mod init dispatch &>>"$LOGFILE"
 VALIDATE $? "init dispatch "
-go get &>> "$LOGFILE"
+go get &>>"$LOGFILE"
 VALIDATE $? "go get app"
-go build &>> "$LOGFILE"
+go build &>>"$LOGFILE"
 VALIDATE $? "build go app"
 #We need to setup a new service in systemd so systemctl can manage this service
 
 #Setup SystemD Dispatch Service
 
-{ cp /root/roboshop-shell/configuration/dispatch.service /etc/systemd/system/dispatch.service; } &>> "$LOGFILE"
+{ cp /root/roboshop-shell/configuration/dispatch.service /etc/systemd/system/dispatch.service; } &>>"$LOGFILE"
 VALIDATE $? "creation of dispatch service"
 
 #Load the service.
 
-systemctl daemon-reload &>> "$LOGFILE"
+systemctl daemon-reload &>>"$LOGFILE"
 VALIDATE $? "reload daemon "
 #Start the service.
 
-systemctl enable dispatch &>> "$LOGFILE"
+systemctl enable dispatch &>>"$LOGFILE"
 VALIDATE $? "enable dispatch "
-systemctl start dispatch &>> "$LOGFILE"
+systemctl start dispatch &>>"$LOGFILE"
 VALIDATE $? "start dispatch "
 echo -e "$Y dispatch Application installed successfully $N"
